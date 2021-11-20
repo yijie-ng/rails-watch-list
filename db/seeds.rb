48cc2f553b3bdb500798c4d3aed7695dda5ff36b
Yi Jie Ng ngyijie92@gmail.com <ngyijie92@gmail.com>
2021-11-20 14:12:51 +0800

    Created lists controller, created list index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+movie_json = JSON.parse(URI.open(url).read)
+results = movie_json['results']
+
+puts 'cleaning up database...'
+Movie.destroy_all
+puts 'Database is clean!'
+puts 'Creating database...'
+results.each do |result|
+  movie = Movie.create!(
+    title: result['original_title'],
+    overview: result['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{result['poster_path']}",
+    rating: result['vote_average']
+  )
+  puts "Movie #{movie.id} is created!"
+end
+
+puts 'Done!'
